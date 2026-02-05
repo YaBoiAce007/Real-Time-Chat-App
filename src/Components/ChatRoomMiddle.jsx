90bfe052e405444a4beb7200c109ed22f2f0fa41
Aniketh Gurung <anikethg68@gmail.com>
2026-02-05 20:45:16 +0530

    updated index.css

--- src/Components/ChatRoomMiddle.jsx
+++ src/Components/ChatRoomMiddle.jsx
@@ -1,8 +1,15 @@
 
 
 function ChatRoomMiddle(){
+
+    const style = {
+        height: '80%',
+        width: '100%',
+        border: '2px solid white'
+    }
+
     return(
-        <div>
+        <div style={style} className={`flex-col`}>
 
         </div>
     )
--- src/Components/ChatRoomsBottom.jsx
+++ src/Components/ChatRoomsBottom.jsx
@@ -1,8 +1,20 @@
 
 
 function ChatRoomsBottom(){
+
+    const style = {
+        height: '90%',
+        width: '100%',
+        //Reminder for my future self, use these styles below for each ChatCard :)
+        /*whiteSpace: 'nowrap',
+        overflowX: 'hidden',
+        textOverflow: 'ellipsis'
+        */
+       border: '2px solid white'
+    }
+    
     return(
-        <div className={`flex-col`}>
+        <div style={style} className={`flex-col`}>
             
         </div>
     )
